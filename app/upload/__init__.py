42646fe15e2363534aa7b651fa9e034a2a41a428
Mitesh Shah <miteshninja@gmail.com>
2016-05-29 00:39:14 +0530

    Removed count_file

--- app/upload/__init__.py
+++ app/upload/__init__.py
@@ -2,7 +2,3 @@ from flask import Flask
 from app import app
 from app.upload import views
 from app.upload import models
-
-# Makes sure the file exists.
-with open(app.config['COUNT_FILE'], 'a') as f:
-    pass
