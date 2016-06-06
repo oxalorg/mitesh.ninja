dab0b016a9b897494a6c71fea01832b37724b18b
Mitesh Shah <miteshninja@gmail.com>
2016-06-06 20:14:49 +0530

    Added flask-migrate.

---
+++ manage.py
@@ -0,0 +1,12 @@
+from app import app, db
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
