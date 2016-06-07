bf2dff6e829527d4c6458d1f495196fe50db51b8
Mitesh Shah <miteshninja@gmail.com>
2016-06-07 20:41:49 +0530

    Config error was still not fixed. Fixed now.

--- conf/setConf.sh
+++ conf/setConf.sh
@@ -8,7 +8,7 @@ sudo cp -iv $basedir/mitesh.ninja.conf $nginxdir/mitesh.ninja.conf
 sudo cp -iv $basedir/mitesh.ninja.uwsgi.ini $vassaldir/mitesh.ninja.uwsgi.ini
 
 # restart the uwsgi vassal process
-sudo touch $vassaldir/mitesh.ninja.uwsgi.conf
+sudo touch $vassaldir/mitesh.ninja.uwsgi.ini || echo "uwsgi touch failed."
 
 # check nginx config and restart
 sudo nginx -t && echo "Now RUN: sudo service nginx restart" || echo "nginx config error"
