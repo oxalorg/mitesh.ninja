3b1bbec95a0eeefe375a3717239b03deaa7ca226
Mitesh Shah <miteshninja@gmail.com>
2016-06-12 09:58:46 +0530

    added analytics

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    print(e)
     return render_template('404.html'), 404
