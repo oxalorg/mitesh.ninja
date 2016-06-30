86e3bbae80f33f7513066007d141d37dbe27e6d2
Mitesh Shah <mitesh@miteshshah.com>
2016-06-30 18:05:40 +0530

    Fixed google verification.

--- app/main.py
+++ app/main.py
@@ -10,3 +10,9 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route('/' + app.config['GOOGLE_VERIFY'])
+@app.route('/' + app.config['GOOGLE_VERIFY'] + '.html')
+def google_verify():
+    return 'google-site-verification: ' + app.config['GOOGLE_VERIFY'] + '.html'
