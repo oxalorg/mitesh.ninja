764300589752ae8a22f2b81c21fb734de84bb7b8
Mitesh Shah <mitesh@miteshshah.com>
2016-08-26 00:58:31 +0530

    easier remote updating script

--- conf/setConf.sh
+++ conf/setConf.sh
@@ -12,3 +12,12 @@ sudo touch $vassaldir/mitesh.ninja.uwsgi.ini || echo "uwsgi touch failed."
 
 # check nginx config and restart
 sudo nginx -t && echo "Now RUN: sudo service nginx restart" || echo "nginx config error"
+
+while true; do
+    read -p "Do you wish to reload nginx?" yn
+    case $yn in
+        [Yy]* ) sudo nginx -s reload; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer Y or N";;
+    esac
+done
--- remote-pull.sh
+++ remote-pull.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/venv sh
-ssh -t mitu@server1.mitesh.ninja "cd /var/www/mitesh.ninja/public/ && git pull github master || echo 'Something went wrong'"
+ssh -t mitu@server1.mitesh.ninja "cd /var/www/mitesh.ninja/public/ && git pull github master && cd conf && sh setConf.sh || echo 'Something went wrong'"
